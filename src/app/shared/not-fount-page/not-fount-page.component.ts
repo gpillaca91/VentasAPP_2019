461a6dc0c991608a4079e3adc0c23147431d39f4
Gianmarco Pillaca <gpillaca91@gmail.com>
2019-06-10 22:57:45 -0500

    Update plantilla html y components v1.1.0

--- src/app/shared/not-fount-page/not-fount-page.component.ts
+++ src/app/shared/not-fount-page/not-fount-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-not-fount-page',
   templateUrl: './not-fount-page.component.html',
-  styles: []
+  styleUrls: ['./not-fount-page.component.css']
 })
 export class NotFountPageComponent implements OnInit {
 
